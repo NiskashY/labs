b3015660b2c4a6f76a33d9d1ad2a8eb74f134f37
NiskashY <roman.pantus@mail.ru>
2021-10-28 19:48:11 +0300

    add functions/dont fix problem with last even groups

--- lab7/lab7.cpp
+++ lab7/lab7.cpp
@@ -1,47 +1,64 @@
+/*В строке, состоящей из групп нулей и единиц,
+найти и вывести на экран группы с четным количеством символов*/
+
 #include <iostream>
 #include <string>
 using namespace std;
+
+
+bool isStringCorrect(string);
+void countEvenGroups(string);
+
 int main()
 {
-	string str;
-	int count = 0;
-	bool flag = false;
+	string str; 
+
 	cout << "String: ";
+
 	do {
 		getline(cin, str);
-		for (int i = 0; i < str.size(); i++) 
-		{
-			if (str[i] != '0' && str[i] != '1' && str[i] != ' ')
-			{
-				cout << "You have entered the string that is not only from 0 and 1!\tEnter the string again:\n";
-				flag = true;
-				break;
-			}
-			else flag = false ;
-		}
-	} while (flag);
+	} while (isStringCorrect(str));
+
 	cout << endl << "Groups with even amount of elemets: ";
-	for (int i = 0, j = 0; i < str.size(); i++)
+	countEvenGroups(str);
+
+	cout << endl;
+	return 0;
+}
+
+bool isStringCorrect(string s)
+{
+	for (int index = 0; index < s.size(); index++)
 	{
-		if (str[i] == ' ')
+		if (s[index] != '0' && s[index] != '1' && s[index] != ' ')
 		{
-			if (count % 2 == 0)
+			cout << "You have entered the string that is not only from 0 and 1!\tEnter the string again:\n";
+			return true;
+		}
+	}
+	return false;
+}
+void countEvenGroups(string str)
+{
+	for (int index = 0, index_after_space = 0, amount = 0; index < str.size(); index++)
+	{
+		if (str[index] == ' ' || index == str.size() - 1)
+		{
+			if (amount % 2 == 0)
 			{
-				for (j; j < i; j++)
+				for (index_after_space; index_after_space < index; index_after_space++)
 				{
-					cout << str[j] ;
+					cout << str[index_after_space];
+
 				}
 				cout << ' ';
-				
 			}
-			j = i + 1;
-			count = 0;
+			index_after_space = index + 1;
+			amount = 0;
 		}
 		else
 		{
-			count++;
+			amount++;
 		}
 	}
-	cout << endl;
-	return 0;
 }

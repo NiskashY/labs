ab6e703bbb53beaf82a5e006aaf5d138d69e857e
NiskashY <roman.pantus@mail.ru>
2021-11-29 11:42:34 +0300

    Tolik

--- lab7/lab7.cpp
+++ lab7/lab7.cpp
@@ -7,6 +7,7 @@ using namespace std;
 
 bool isStringCorrect(char*);
 void countEvenGroups(char*);
+int My_strlen(char*);
 
 int main()
 {
@@ -25,39 +26,63 @@ int main()
 	return 0;
 }
 
+int My_strlen(char* str) {
+	int length = 0;
+	for (length; str[length]; ++length);
+	return length;
+}
 bool isStringCorrect(char* str)
 {
-	for (int index = 0; index <= strlen(str); index++)
+	int len = My_strlen(str);
+	for (int index = 0; index <= len; index++)
 	{
-		if (str[index] != '0' && str[index] != '1' && str[index] != ' ' && str[index] != '\0')
+		if (str[index] != '0' && str[index] != '1' && str[index] && str[index] != '\0')
 		{
 			cout << "You have entered the string that is not only from 0 and 1!\tEnter the string again:\n";
 			return true;
 		}
 	}
 	return false;
-}
+}/*
 void countEvenGroups(char* str)
 {
-	for (int index = 0, index_after_space = 0, amount = 0; index <= strlen(str); index++)
+	int len = My_strlen(str);
+	for (int index = 0, index_after_space = 0, amount = 0; index <= len; index++)
 	{
-		if (str[index] == ' ' || str[index] == '\0')
+		if (index != 0 && str[index] != str[index - 1])
 		{
-			if (amount % 2 == 0 && amount )
+			if (amount % 2 == 0 && amount)
 			{
 				for (index_after_space; index_after_space < index; index_after_space++)
 				{
 					cout << str[index_after_space];
-
 				}
 				cout << ' ';
 			}
-			index_after_space = index + 1;
+			index_after_space = index;
 			amount = 0;
 		}
-		else
+		amount++;
+	}
+}*/
+void countEvenGroups(char* str)
+{
+	int len = My_strlen(str);
+	char* pstr = str;
+	for (int index = 0, index_after_space = 0, amount = 0; index <= len; index++)
+	{
+		if (index != 0 && str[index] != str[index - 1])
 		{
-			amount++;
+			if (amount % 2 == 0 && amount)
+			{
+				cout << str[index - 1];
+				str[index - 1] = '\0';
+				cout << pstr << ' ';
+			}
+			pstr = &str[index];
+			index_after_space = index;
+			amount = 0;
 		}
+		amount++;
 	}
 }

d371005ab9496fb9e70e7624603aa20ba70b1411
NiskashY <roman.pantus@mail.ru>
2021-12-01 13:25:19 +0300

    update (not sure)

--- lab7/lab7.cpp
+++ lab7/lab7.cpp
@@ -2,22 +2,23 @@
 найти и вывести на экран группы с четным количеством символов*/
 
 #include <iostream>
+#include <cstdio>
+#include <conio.h>
 
 using namespace std;
 
 bool isStringCorrect(char*);
 void countEvenGroups(char*);
 int My_strlen(char*);
+void MyGets(char*);
 
 int main()
 {
-	char str[100] = "";
+	char str[1000] = "";
 	cout << "String: ";
-
 	do {
-		gets_s(str);
+		MyGets(str);
 	} while (isStringCorrect(str));
-
 	cout << endl << "Groups with even amount of elements: ";
 	countEvenGroups(str);
 
@@ -26,6 +27,16 @@ int main()
 	return 0;
 }
 
+void MyGets(char* str) {
+	char input;
+	do {
+		input = _getch();
+		cout << input;
+		if (input == '\r') break;
+		*str = input;
+		str++;
+	} while (true);
+}
 int My_strlen(char* str) {
 	int length = 0;
 	for (length; str[length]; ++length);
@@ -36,35 +47,14 @@ bool isStringCorrect(char* str)
 	int len = My_strlen(str);
 	for (int index = 0; index <= len; index++)
 	{
-		if (str[index] != '0' && str[index] != '1' && str[index] && str[index] != '\0')
+		if (str[index] != '0' && str[index] != '1' && str[index] != '\0')
 		{
 			cout << "You have entered the string that is not only from 0 and 1!\tEnter the string again:\n";
 			return true;
 		}
 	}
 	return false;
-}/*
-void countEvenGroups(char* str)
-{
-	int len = My_strlen(str);
-	for (int index = 0, index_after_space = 0, amount = 0; index <= len; index++)
-	{
-		if (index != 0 && str[index] != str[index - 1])
-		{
-			if (amount % 2 == 0 && amount)
-			{
-				for (index_after_space; index_after_space < index; index_after_space++)
-				{
-					cout << str[index_after_space];
-				}
-				cout << ' ';
-			}
-			index_after_space = index;
-			amount = 0;
-		}
-		amount++;
-	}
-}*/
+}
 void countEvenGroups(char* str)
 {
 	int len = My_strlen(str);
@@ -86,3 +76,24 @@ void countEvenGroups(char* str)
 		amount++;
 	}
 }
+/*void countEvenGroups(char* str)
+{
+	int len = My_strlen(str);
+	for (int index = 0, index_after_space = 0, amount = 0; index <= len; index++)
+	{
+		if (index != 0 && str[index] != str[index - 1])
+		{
+			if (amount % 2 == 0 && amount)
+			{
+				for (index_after_space; index_after_space < index; index_after_space++)
+				{
+					cout << str[index_after_space];
+				}
+				cout << ' ';
+			}
+			index_after_space = index;
+			amount = 0;
+		}
+		amount++;
+	}
+}*/

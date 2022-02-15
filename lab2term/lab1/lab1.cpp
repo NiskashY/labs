8cbe77d5ef1a725be346d67290d89d9527310788
NiskashY <roman.pantus@mail.ru>
2022-02-15 11:37:07 +0300

    (#lab 1) done like bsearch

--- lab2term/lab1/lab1.cpp
+++ lab2term/lab1/lab1.cpp
@@ -3,6 +3,7 @@
 #include <iomanip>
 
 double MinSearch(double& a, double& b, double& e);
+double loop(double& a, double& b, double& e);
 bool IsALessB(double a, double b);
 void InputAB(double& a, double& b);
 double InputNum();
@@ -14,12 +15,14 @@ int main() {
     InputAB(a, b);
     e = InputNum();
 
-    std::cout << std::fixed << std::setprecision(15) << MinSearch(a, b, e);
+    std::cout << std::fixed << std::setprecision(15) << MinSearch(a, b, e) << std::endl;
+    std::cout << std::fixed << std::setprecision(15) << loop(a, b, e) << std::endl;
+
 
     return 0;
 }
 
-
+/*
 double MinSearch(double& a, double& b, double& e) {
     /// данна€ переменна€ объ€вл€етс€ только один раз,
     /// а далее она измен€етс€ и видна только в пределах этой функции
@@ -35,6 +38,50 @@ double MinSearch(double& a, double& b, double& e) {
     }
     a += e;
     MinSearch(a, b, e);
+
+    return min;
+}*/
+
+double MinSearch(double& a, double& b, double& e) {
+    static double min = 7 * sin(a) * sin(a);
+
+    if (b - a <= e) {
+        return min;
+    }
+
+    double mid = (b + a) / 2;
+    double left = 7 * sin(mid - e) * sin(mid - e);
+    double right = 7 * sin(mid + e) * sin(mid + e);
+
+    if (left < right) {
+        min = left;
+        MinSearch(a, mid, e);
+    }
+    else {
+        min = right;
+        MinSearch(mid, b, e);
+    }
+
+    return min;
+}
+
+double loop(double& a, double &b, double& e){
+    double min = 7 * sin(a) * sin(a);
+
+    while (b - a > e) {
+        double mid = (b + a) / 2;
+        double left = 7 * sin(mid - e) * sin(mid - e);
+        double right = 7 * sin(mid + e) * sin(mid + e);
+
+        if (left < right) {
+            min = left;
+            b = mid;
+        }
+        else {
+            min = right;
+            a = mid;
+        }
+    }
 
     return min;
 }

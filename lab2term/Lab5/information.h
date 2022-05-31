037389fddc5ff1496dcdb0bbc2ca632d44008429
NiskashY <roman.pantus@mail.ru>
2022-05-31 06:12:59 +0300

    (#lab5) refactor. view always on display, add test3, template CheckNum, some comments.

--- lab2term/Lab5/information.h
+++ lab2term/Lab5/information.h
@@ -7,7 +7,14 @@ struct Information {
 	std::string favorite_color;
 };
 
-double CheckNum();
+template <class T>
+void CheckNum(T& var) {
+	while (!(std::cin >> var) || std::cin.get() != '\n') {
+		std::cout << "Error! Something go wrong ReEnter: ";
+		std::cin.clear();
+		while (std::cin.get() != '\n');
+	}
+}
 
 std::istream& operator>>(std::istream&, Information&);
 
--- lab2term/Lab5/tests.cpp
+++ lab2term/Lab5/tests.cpp
@@ -4,65 +4,68 @@ void RunTests() {
 	Test1();
 	std::cout << '\n';
 	Test2();
+	std::cout << '\n';
+	Test3();
 }
 
 void Test1() {
 	std::cout << "-------TEST1----\n\n";
 
-	std::cout << "\x1b[31m" << "WARNING: You want to run this test? It will take about 2 min to complete test. YES - 3 ( hehe :) ), NO - else:\x1b[0m ";
-	int choice = CheckNum();
+	std::cout << "\x1b[31m" << "WARNING: You want to run this test? It will take about 2 min to complete test. YES - 3, NO - else:\x1b[0m ";
+	int choice = 0;
+	CheckNum(choice);
 	if (choice != 3) {
 		std::cout << "Skipped!\n";
 		return;
 	}
 
-	Tree* tree = new Tree();
-	int amount = 1 << 16 - 1;
+	Tree tree;
+	int amount = (1 << 15) - 1;
 	clock_t start = clock();
 
 	for (int i = 0; i < amount; ++i) {
 		Information inf = { i, "" };
-		tree->insert(inf);
+		tree.insert(inf);
 	}
 	clock_t end = clock();
 	double seconds = (double)(end - start) / CLOCKS_PER_SEC;
 	std::cout << "Insert seconds: " << seconds << '\n';
 
-	int max_height = GetHeight(tree->GetRoot());
+	int max_height = GetHeight(tree.GetRoot());
 
 	std::cout << max_height << "\n\n";
 
 	start = clock();
-	tree->view();
+	tree.view();
 	end = clock();
 	seconds = (double)(end - start) / CLOCKS_PER_SEC;
 	std::cout << "View seconds: " << seconds << '\n';
 
 	std::cout << "Show LEFT-ROOT-RIGHT: ";
-	ShowLeftRootRight(tree->GetRoot());
+	ShowLeftRootRight(tree.GetRoot());
 	std::cout << '\n';
 	std::cout << "Show ROOT-LEFT-RIGHT: ";
-	ShowRootLeftRight(tree->GetRoot());
+	ShowRootLeftRight(tree.GetRoot());
 	std::cout << '\n';
 	std::cout << "Show LEFT-RIGHT-ROOT: ";
-	ShowLeftRightRoot(tree->GetRoot());
+	ShowLeftRightRoot(tree.GetRoot());
 	std::cout << '\n';
 
 	Information inf = { rand() % (1 << 16), "" };
-	tree->search(inf);
-	tree->minKey();
-	tree->maxKey();
+	tree.search(inf);
+	tree.minKey();
+	tree.maxKey();
 	Information inf1 = { rand() % (1 << 16), "" };
-	tree->remove(inf1);
+	tree.remove(inf1);
 
-	tree->view();
+	tree.view();
 }
 
 void Test2() {
 	std::cout << "-------TEST2----\n\n";
 
-	Tree* tree = new Tree();
-	int amount = 100;
+	Tree tree;
+	int amount = 10;
 	clock_t start = clock();
 
 	srand(time(NULL));
@@ -70,34 +73,63 @@ void Test2() {
 	for (int i = 0; i < amount; ++i) {
 		int tmp = rand() % 100 + 1;
 		Information inf = { tmp, "" };
-		tree->insert(inf);
+		tree.insert(inf);
 	}
 
 	clock_t end = clock();
 	double seconds = (double)(end - start) / CLOCKS_PER_SEC;
 	std::cout << "Insert seconds: " << seconds << '\n';
-	int max_height = GetHeight(tree->GetRoot());
+	int max_height = GetHeight(tree.GetRoot());
 	std::cout << "Height = " << max_height << " Amount of elements = " << amount << "\n\n";
 	start = clock();
-	tree->view();
+	tree.view();
 	end = clock();
 	seconds = (double)(end - start) / CLOCKS_PER_SEC;
 	std::cout << "View seconds: " << seconds << '\n';
 
 	std::cout << "Show LEFT-ROOT-RIGHT: ";
-	ShowLeftRootRight(tree->GetRoot());
+	ShowLeftRootRight(tree.GetRoot());
 	std::cout << '\n';
 	std::cout << "Show ROOT-LEFT-RIGHT: ";
-	ShowRootLeftRight(tree->GetRoot());
+	ShowRootLeftRight(tree.GetRoot());
 	std::cout << '\n';
 	std::cout << "Show LEFT-RIGHT-ROOT: ";
-	ShowLeftRightRoot(tree->GetRoot());
+	ShowLeftRightRoot(tree.GetRoot());
 	std::cout << '\n';
 	Information inf = { rand() % 100 + 1, "" };
-	tree->search(inf);
-	tree->minKey(true);
-	tree->maxKey(true);
+	tree.search(inf);
+	tree.minKey(true);
+	std::cout << '\n';
+	tree.maxKey(true);
+	std::cout << '\n';
 	Information inf1 = { rand() % 100 + 1, "" };
-	tree->remove(inf1);
-	tree->view();
+	tree.remove(inf1);
+	tree.view();
+}
+
+void Test3() {
+	std::cout << "-------TEST3----\n\n";
+
+	Tree tree;
+	int amount = 20;
+	clock_t start = clock();
+
+	srand(time(NULL));
+
+	for (int i = 0; i < amount; ++i) {
+		int tmp = rand() % 100 + 1;
+		Information inf = { tmp, "" };
+		tree.insert(inf);
+	}
+
+	clock_t end = clock();
+	double seconds = (double)(end - start) / CLOCKS_PER_SEC;
+	std::cout << "Insert seconds: " << seconds << '\n';
+	int max_height = GetHeight(tree.GetRoot());
+	std::cout << "Height = " << max_height << " Amount of elements = " << amount << "\n\n";
+	start = clock();
+	tree.view();
+	end = clock();
+	seconds = (double)(end - start) / CLOCKS_PER_SEC;
+	std::cout << "View seconds: " << seconds << '\n';
 }
